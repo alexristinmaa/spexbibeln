3d9cdaae2bc6d3d984661349754896e897a13099
matrixthecow <alexander.ristinmaa@icloud.com>
2025-08-07 21:40:57 +0200

    Inital commit

---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkdb8Y_skk2fD6VTFCCfUausDaJ4A5xRI",
+  authDomain: "spexbibeln.firebaseapp.com",
+  projectId: "spexbibeln",
+  storageBucket: "spexbibeln.firebasestorage.app",
+  messagingSenderId: "800168760255",
+  appId: "1:800168760255:web:48be29d4b1aee00b53c70a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
